b10df4cf0ce5c207445a3fae1dc4a6ab31a1dc78
Riadh-Aouameur <riadhaouameur02@gmail.com>
2019-12-22 08:55:26 +0100

    add picture

---
+++ src/main/java/Medical/Main.java
@@ -0,0 +1,52 @@
+package Medical;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        System.out.println("hello world");
+        primaryStage.setTitle("Hello World!");
+        Image image = new Image("Medical/dr.png");
+        Stage a = new Stage();
+        a.setTitle("Doctor");
+        a.setScene(new Scene(new StackPane()));
+
+        // primaryStage.resizableProperty().setValue(Boolean.FALSE);
+        //  primaryStage.initStyle(StageStyle.UTILITY);
+        //primaryStage.initStyle(StageStyle.UTILITY);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+
+        ImageView iv1 = new ImageView();
+        iv1.setFitHeight(300);
+        iv1.setFitWidth(300);
+        iv1.setImage(image);
+        iv1.setOnMouseClicked(event -> {
+            a.show();
+            primaryStage.close();
+        });
+
+        HBox box = new HBox();
+        box.getChildren().add(iv1);
+
+
+        StackPane root = new StackPane();
+        root.getChildren().add(box);
+
+        primaryStage.setScene(new Scene(root, 300, 350));
+        primaryStage.show();
+
+    }
+}
